c20bd27d551c5cb93ea9c197e744b0f8f0d5abec
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2023-11-06 16:29:06 -0300

    semifinal

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3276.2258940471197!2d-58.35577382425123!3d-34.80025827288442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32ca3750492c1%3A0xc15f2a9d46ba7f90!2sVial%20Argentina%20Eventos!5e0!3m2!1ses-419!2sar!4v1698326300171!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3200.745260457816!2d-64.3410742!3d-36.6565785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95dd333478215171%3A0x5d01c1027239b3b!2sHornero%205847%2C%20Toay%2C%20La%20Pampa!5e0!3m2!1ses-419!2sar!4v1699297991183!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         
         
     })
